5fc2b1e0a5fb6339be6469b77c2602a4ed0573ce
g00402163 <g00402163@atu.ie>
2022-12-12 10:17:17 +0000

    Ship

--- src/main/java/Ferry.java
+++ src/main/java/Ferry.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Ferry {
+import java.security.PublicKey;
+
+public class Ferry extends Ship {
+    private String FerryNumber;
+    private boolean mailingList;
+
+    public Ferry(String name, String build, String crew) {
+        super(name, build, crew);
+    }
+
+    public Ferry() {
+        Public Ferry(String name, String built, String crew boolean mailingList) {
+            super(name,built,crew);
+            this.FerryNumber = FerryNumber;
+            this.mailingList = mailingList;
+        }
+
+    }
 }
--- src/main/java/Ship.java
+++ src/main/java/Ship.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class Ship {
+package ie.atu.XmasExam;
+
+public class Ship {
+
+    private String name;
+    private String build;
+    private String crew;
+
+    public Ship() {
+    }
+
+    public Ship(String name, String build, String crew) {
+        this.name = name;
+        this.build = build;
+        this.crew = crew;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBuild() {
+        return build;
+    }
+
+    public void setBuild(String build) {
+        this.build = build;
+    }
+
+    public String getCrew() {
+        return crew;
+    }
+
+    public void setCrew(String crew) {
+        this.crew = crew;
+    }
+    public static void main(String[] args) {
+        Ship Sandrius = new Ship();
+        Sandrius.setName("Elizabeth");
+        Sandrius.setBuild("2003");
+        Sandrius.setCrew("196");
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
